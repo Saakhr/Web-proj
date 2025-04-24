f0815c867ba94cdcbfe628f189d66f0f0a09af81
maamoun <SakhrOrg@proton.me>
2025-04-24 21:54:47 +0100

    db fix

--- pkg/database/db.go
+++ pkg/database/db.go
@@ -28,27 +28,27 @@ func createTables() error {
 	tables := []string{
 		`CREATE TABLE IF NOT EXISTS admins (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			email TEXT NOT NULL,
+			email TEXT NOT NULL UNIQUE,
 			password TEXT NOT NULL,
 			full_name TEXT NOT NULL
 		);`,
 		`CREATE TABLE IF NOT EXISTS announcements (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			title TEXT NOT NULL,
+			title TEXT NOT NULL UNIQUE,
 			content TEXT NOT NULL,
 			display TEXT NOT NULL CHECK(display IN ('general', 'computer_science', 'physics', 'chemistry', 'math')),
 			datetime DATETIME NOT NULL
 		);`,
 		`CREATE TABLE IF NOT EXISTS projects (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			title TEXT NOT NULL,
+			title TEXT NOT NULL UNIQUE,
 			description TEXT NOT NULL
 		);`,
 		`CREATE TABLE IF NOT EXISTS students (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
 			first_name TEXT NOT NULL,
 			last_name TEXT NOT NULL,
-			email TEXT NOT NULL,
+			email TEXT NOT NULL UNIQUE,
 			password TEXT NOT NULL
 		);`,
 		`CREATE TABLE IF NOT EXISTS student_project_wishlist (
@@ -56,7 +56,8 @@ func createTables() error {
 			student_id INTEGER NOT NULL,
 			project_id INTEGER NOT NULL,
 			FOREIGN KEY (student_id) REFERENCES students(id) ON DELETE CASCADE,
-			FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE
+			FOREIGN KEY (project_id) REFERENCES projects(id) ON DELETE CASCADE,
+      UNIQUE (student_id, project_id)
 		);`,
 	}
 
